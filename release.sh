79553be4a97698efb138b788623e40c738a6e65c
claytonsilva <clayton@xdevel.com.br>
2017-05-06 11:53:59 -0300

    ci(release.sh): ajustado release.sh para evitar release duplicada
    :D

--- release.sh
+++ release.sh
@@ -45,26 +45,3 @@ mv -f _package.json package.json &&
 npm version ${1:-$bump} -m "chore(release): %s" &&
 git push --follow-tags &&
 conventional-github-releaser -p ${2:-$preset}
-echo "Apagando node_modules" &&
-rimraf node_modules &&
-echo "git pull --rebase" &&
-git pull --rebase &&
-echo "yarn install" &&
-yarn install &&
-echo "yarn test" &&
-yarn test &&
-cp package.json _package.json &&
-preset=$(conventional-commits-detector) &&
-echo ${2:-$preset} &&
-bump=$(conventional-recommended-bump -p ${2:-$preset}) &&
-echo ${1:-$bump} &&
-npm --no-git-tag-version version ${1:-$bump} &&
-conventional-changelog -i CHANGELOG.md -s -p ${2:-$preset} &&
-git add CHANGELOG.md &&
-version=$(json -f package.json version) &&
-echo ${3:-$version} &&
-git commit -m"docs(CHANGELOG): $version" &&
-mv -f _package.json package.json &&
-npm version ${1:-$bump} -m "chore(release): %s" &&
-git push --follow-tags &&
-conventional-github-releaser -p ${2:-$preset}
