f6a9b2188273f8c99e0f5021f899dc4d6c2180d0
claytonsilva <clayton@xdevel.com.br>
2017-05-11 11:45:03 -0300

    chore(release.sh): teste de versao

--- release.sh
+++ release.sh
@@ -38,16 +38,19 @@ preset=$(conventional-commits-detector) &&
 echo ${2:-$preset} &&
 bump=$(conventional-recommended-bump -p ${2:-$preset}) &&
 echo ${1:-$bump} &&
-version=$(json -f package.json version) &&
-echo ${3:-$version} &&
 npm --no-git-tag-version version ${1:-$bump} &&
 conventional-changelog -i CHANGELOG.md -s -p ${2:-$preset} &&
 git add CHANGELOG.md &&
+version=$(json -f package.json version) &&
+echo ${3:-$version} &&
 git commit -m"docs(CHANGELOG): $version" &&
+git flow release start $version
 mv -f _package.json package.json &&
 npm version ${1:-$bump} -m "chore(release): %s" &&
 conventional-github-releaser -p ${2:-$preset} &&
+git checkout develop && 
+git rebase --no-ff release/$version &&
 git checkout master &&
-git merge develop &&
+git rebase --no-ff release/$version &&
 git push origin develop master --follow-tags
 git checkout develop
