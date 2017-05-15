16697e27882e4bd35c361cf34f00a4cdd59d4741
claytonsilva <clayton@xdevel.com.br>
2017-05-15 02:41:52 -0300

    chore(release.sh): teste de versao

--- release.bash
+++ release.bash
@@ -68,7 +68,7 @@ function prerelease {
 	oldVersion=$(json -f package.json version) 
 	version=$(semver ${oldVersion}  --no-git-tag-version -i ${2:-$bump} --preid rc)
 	echo 'testando versao nova com prerelease'
-	echo $version
+	semver ${oldVersion}  --no-git-tag-version -i ${2:-$bump} --preid rc
 	echo 'testando versao nova com prerelease'
 	npm --no-git-tag-version version ${version} 
 	conventional-changelog -i CHANGELOG.md -s -p ${3:-$preset} 
